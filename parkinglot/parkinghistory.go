20bea2ebc68ef9593344577ff5c971bf515231fa
Ahmad Umar <ahmad.umar.byl@gmail.com>
2025-04-20 00:40:01 +0700

    feat(parkinghistory): add sync.rw mutex  for thread safety

--- parkinglot/parkinghistory.go
+++ parkinglot/parkinghistory.go
@@ -1,10 +1,13 @@
 package parkinglot
 
 import (
+	"sync"
+
 	"github.com/umerthow/parking-lot-go/model"
 )
 
 type VehicleHistory struct {
+	mu   sync.RWMutex
 	Data map[string][]model.ParkingSpot
 }
 
@@ -15,10 +18,16 @@ func NewVehicleHistory() *VehicleHistory {
 }
 
 func (vh *VehicleHistory) Record(vehicleNumber string, spot model.ParkingSpot) {
+	vh.mu.Lock()
+	defer vh.mu.Unlock()
+
 	vh.Data[vehicleNumber] = append(vh.Data[vehicleNumber], spot)
 }
 
 func (vh *VehicleHistory) Get(vehicleNumber string) ([]model.ParkingSpot, bool) {
+	vh.mu.RLock()
+	defer vh.mu.RUnlock()
+
 	history, exists := vh.Data[vehicleNumber]
 	return history, exists
 }
